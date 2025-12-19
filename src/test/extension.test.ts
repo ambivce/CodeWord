90c2c723bad75f8661753c3d50608538237f4a0a
ambivce <ambivce@gmail.com>
2025-12-19 08:57:51 +0100

    feat: added tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,49 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Extension Activation", () => {
+  test("Extension should activate", async () => {
+    const extension = vscode.extensions.getExtension("ambivce.code-word");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    assert.ok(extension, "Extension not found");
+
+    await extension!.activate();
+
+    assert.strictEqual(extension!.isActive, true);
+  });
+
+  test("Meditation mode can be set", async () => {
+    const config = vscode.workspace.getConfiguration("CodeWord");
+
+    await config.update(
+      "mode",
+      "Meditation",
+      vscode.ConfigurationTarget.Global
+    );
+
+    const mode = config.get("mode");
+    assert.strictEqual(mode, "Meditation");
+  });
+
+  test("Command is registered", async () => {
+    const commands = await vscode.commands.getCommands(true);
+
+    assert.ok(
+      commands.includes("code-word.unsheath"),
+      "Command not registered"
+    );
+  });
+
+  test("Command executes without error", async () => {
+    try {
+      vscode.commands.executeCommand("code-word.unsheath");
+      assert.ok(true);
+    } catch (err) {
+      assert.fail("Command threw an error");
+    }
+  });
 });
